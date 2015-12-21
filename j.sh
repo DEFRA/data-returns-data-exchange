9d700e82482581dd7a901e3e2f9a7a01a3936830
adrian harrison <adrian.harrison.ea@gmail.com>
2015-12-21 09:39:39 +0000

    updated to kill existing process first

--- j.sh
+++ j.sh
@@ -1,5 +1,6 @@
 export JAVA_HOME=/opt/jdk1.8.0_45
 export PATH=$JAVA_HOME/bin:$PATH
+pkill java
 java -version
-nohup java -jar data-returns-data-exchange-1.0-SNAPSHOT.one-jar.jar server configuration_local.yml &
-
+nohup java -jar target/data-returns-data-exchange-1.0-SNAPSHOT.one-jar.jar server configuration_local.yml &
+tail -f nohup.out
