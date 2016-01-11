dff18ba73a47f006214b91bb0306cdf607f3a640
adrian harrison <adrian.harrison.ea@gmail.com>
2016-01-11 14:34:46 +0000

    Not needed

--- start_server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# TODO needs parameterizing
-
-pkill java
-nohup java -jar /srv/data-returns/data-returns-data-exchange-1.0-SNAPSHOT.one-jar.jar server /srv/data-returns/configuration_dev.yml &
