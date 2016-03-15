c1aebb6cc1cc61acac494d79070046bccda45e1f
Sam Gardner-Dell <sgardnerdell.ea@gmail.com>
2016-03-15 16:57:09 +0000

    Card #350 - Reworked exception codes as per the user journey codes
    Temporary status codes used

--- src/main/java/uk/gov/ea/datareturns/type/AppStatusCodeType.java
+++ src/main/java/uk/gov/ea/datareturns/type/AppStatusCodeType.java
@@ -2,7 +2,7 @@ package uk.gov.ea.datareturns.type;
 
 public enum AppStatusCodeType
 {
-	APP_STATUS_SUCCESS(800, "Success"), 
+	APP_STATUS_SUCCESS(0, "Success"), 
 	APP_STATUS_FAILED_WITH_ERRORS(801, "Failed with errors");
 
 	private int appStatusCode;
