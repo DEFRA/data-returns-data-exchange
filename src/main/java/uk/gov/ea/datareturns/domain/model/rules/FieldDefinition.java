e878cce8dbfddaa70931bca53a8dc9d39c743c7b
graham-willis-druid <graham.willis.ea@gmail.com>
2016-08-15 14:59:12 +0100

    Change to use the dynamic controlled lists in the error table

--- src/main/java/uk/gov/ea/datareturns/domain/model/rules/FieldDefinition.java
+++ src/main/java/uk/gov/ea/datareturns/domain/model/rules/FieldDefinition.java
@@ -14,11 +14,11 @@ public enum FieldDefinition {
 	/** The site name (Site_Name) */
 	Site_Name(null, "http://data-returns-help.herokuapp.com/help/detailed-guides/regime-specific-rules"),
 	/** The return type (Rtn_Type) */
-	Rtn_Type("Return type", "http://data-returns-help.herokuapp.com/help/detailed-guides/return-type"),
+	Rtn_Type("Return type", "/display-list?list=rtn_type"),
 	/** The monitoring date (Mon_Date) */
 	Mon_Date("Monitoring date", "http://data-returns-help.herokuapp.com/help/detailed-guides/regime-specific-rules"),
 	/** The return period (Rtn_Period) */
-	Rtn_Period("Return period", "http://data-returns-help.herokuapp.com/help/detailed-guides/monitoring-period"),
+	Rtn_Period("Return period", "/display-list?list=rtn_period"),
 	/** The monitoring point (Mon_Point) */
 	Mon_Point("Monitor point reference", "http://data-returns-help.herokuapp.com/help/detailed-guides/regime-specific-rules"),
 	/** Sample reference (Smpl_Ref) */
@@ -26,17 +26,17 @@ public enum FieldDefinition {
 	/** Sampled by (Smpl_By) */
 	Smpl_By("Sampled by", "http://data-returns-help.herokuapp.com/help/detailed-guides/regime-specific-rules"),
 	/** Parameter value (Parameter) */
-	Parameter("Parameter", "http://data-returns-help.herokuapp.com/help/detailed-guides/parameters"),
+	Parameter("Parameter", "/display-list?list=parameters"),
 	/** Value (Value) */
 	Value("Value", "http://data-returns-help.herokuapp.com/help/detailed-guides/regime-specific-rules"),
 	/** Textual value (Txt_Value) */
 	Txt_Value("Text value", "http://data-returns-help.herokuapp.com/help/detailed-guides/qualifiers"),
 	/** Unit of measurement (Unit) */
-	Unit(null, "http://data-returns-help.herokuapp.com/help/detailed-guides/units-and-measures"),
+	Unit(null, "/display-list?list=units"),
 	/** Reference period */
-	Ref_Period("Reference period", "http://data-returns-help.herokuapp.com/help/detailed-guides/reference-periods"),
+	Ref_Period("Reference period", "/display-list?list=ref_period"),
 	/** Method or standard used (Meth_Stand) */
-	Meth_Stand("Monitoring method or standard", "http://data-returns-help.herokuapp.com/help/detailed-guides/monitoring-standard"),
+	Meth_Stand("Monitoring method or standard", "/display-list?list=method"),
 	/** Record comments (Comments) */
 	Comments(null, "http://data-returns-help.herokuapp.com/help/detailed-guides/regime-specific-rules"),
 	/** Commercial in confidence data (CiC) */
