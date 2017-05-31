a14e26210c85a6a28132f1a3871ed7a3be22f010
graham-willis-druid <graham.willis.ea@gmail.com>
2017-05-31 10:06:52 +0100

    Fix JPA annotations for the field - validation error join

--- src/main/java/uk/gov/ea/datareturns/domain/jpa/entities/userdata/impl/ValidationError.java
+++ src/main/java/uk/gov/ea/datareturns/domain/jpa/entities/userdata/impl/ValidationError.java
@@ -12,11 +12,21 @@ public class ValidationError {
     @EmbeddedId private ValidationErrorId id;
     @Basic private String message;
 
-    @OneToMany
-    @JoinColumns( {
-            @JoinColumn(name = "payload_type", referencedColumnName = "payload_type"),
-            @JoinColumn(name = "error", referencedColumnName = "error")
-    })
+    /*
+     * Currently does not support maintaining the relationship because the payload_type is shared
+     * by both sides of the association. This means we cannot change the field side of the relationship.
+     * (To do this would require duplication of the payload_type field in the table)
+     */
+    @ManyToMany
+    @JoinTable(name = "validation_error_fields",
+            joinColumns = {
+                @JoinColumn(name = "payload_type", referencedColumnName = "payload_type"),
+                @JoinColumn(name = "error", referencedColumnName = "error")
+            }, inverseJoinColumns = {
+                @JoinColumn(name = "payload_type", referencedColumnName = "payload_type", insertable = false,  updatable = false),
+                @JoinColumn(name = "field_name", referencedColumnName = "field_name")
+        }
+    )
     private Set<Field> fields;
 
     public ValidationErrorId getId() {
