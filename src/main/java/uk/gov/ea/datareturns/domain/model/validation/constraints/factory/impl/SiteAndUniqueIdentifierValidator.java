44e4702df17f0acc6314b8f22ba33b3d8c029b13
graham-willis-druid <graham.willis.ea@gmail.com>
2017-06-23 09:01:45 +0100

    relax site name change

--- src/main/java/uk/gov/ea/datareturns/domain/model/validation/constraints/factory/impl/SiteAndUniqueIdentifierValidator.java
+++ src/main/java/uk/gov/ea/datareturns/domain/model/validation/constraints/factory/impl/SiteAndUniqueIdentifierValidator.java
@@ -7,6 +7,7 @@ import uk.gov.ea.datareturns.domain.model.MessageCodes;
 import uk.gov.ea.datareturns.domain.model.fields.impl.EaId;
 import uk.gov.ea.datareturns.domain.model.fields.impl.SiteName;
 import uk.gov.ea.datareturns.domain.model.validation.constraints.factory.RecordConstraintValidator;
+import uk.gov.ea.datareturns.util.TextUtils;
 
 import javax.validation.ConstraintValidatorContext;
 
@@ -14,6 +15,7 @@ import javax.validation.ConstraintValidatorContext;
  * Created by sam on 11/11/16.
  */
 public class SiteAndUniqueIdentifierValidator implements RecordConstraintValidator<DataSample> {
+
     @Override
     public boolean isValid(DataSample record, final ConstraintValidatorContext context) {
         final EaId eaId = record.getEaId();
@@ -25,15 +27,22 @@ public class SiteAndUniqueIdentifierValidator implements RecordConstraintValidat
         // This gives an unfortunate dependency between the two validations
         // which is unavoidable at this stage. It should really check against the controlled list
         // but cannot do so while being case-insensitive. (There are collisions)
-        if (site.getInputValue() == null) {
-            return true;
+        if (eaIdEntity != null) {
+
+            final String inputSiteName = TextUtils.normalize(site.getInputValue());
+            final String foundSiteName = TextUtils.normalize(eaId.getEntity().getSite().getName());
+
+            if (inputSiteName == null) {
+                return true;
+            }
+
+            if (!StringUtils.equalsIgnoreCase(foundSiteName, inputSiteName)) {
+                context.disableDefaultConstraintViolation();
+                context.buildConstraintViolationWithTemplate(MessageCodes.Conflict.UniqueIdentifierSiteConflict).addConstraintViolation();
+                return false;
+            }
         }
 
-        if (eaIdEntity != null && !StringUtils.equalsIgnoreCase(eaId.getEntity().getSite().getName(), site.getValue())) {
-            context.disableDefaultConstraintViolation();
-            context.buildConstraintViolationWithTemplate(MessageCodes.Conflict.UniqueIdentifierSiteConflict).addConstraintViolation();
-            return false;
-        }
         return true;
     }
 }
