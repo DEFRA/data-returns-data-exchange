5c833b6aaef7e7f35a30a5f51f48165847fac884
graham-willis-druid <graham.willis.ea@gmail.com>
2017-06-30 15:55:54 +0100

    Work in progress on the test stuff

--- src/test/java/uk/gov/ea/datareturns/tests/integration/resource/APIResourceIntegrationTests.java
+++ src/test/java/uk/gov/ea/datareturns/tests/integration/resource/APIResourceIntegrationTests.java
@@ -1,5 +1,6 @@
 package uk.gov.ea.datareturns.tests.integration.resource;
 
+import org.influxdb.com.google.guava.collect.ImmutableMap;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,14 +12,15 @@ import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.ea.datareturns.App;
 import uk.gov.ea.datareturns.tests.integration.api.v1.AbstractDataResourceTests;
 import uk.gov.ea.datareturns.web.resource.v1.model.dataset.Dataset;
-import uk.gov.ea.datareturns.web.resource.v1.model.record.Record;
+import uk.gov.ea.datareturns.web.resource.v1.model.definitions.ConstraintDefinition;
 import uk.gov.ea.datareturns.web.resource.v1.model.record.payload.DataSamplePayload;
 import uk.gov.ea.datareturns.web.resource.v1.model.response.DatasetEntityResponse;
 import uk.gov.ea.datareturns.web.resource.v1.model.response.RecordEntityResponse;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @author Graham Willis
@@ -31,20 +33,77 @@ import static org.assertj.core.api.Assertions.assertThat;
 @ActiveProfiles("IntegrationTests")
 public class APIResourceIntegrationTests extends AbstractDataResourceTests {
 
-    private final static String PAYLOAD_TYPE = "DataSamplePayload";
+    //private final static String PAYLOAD_TYPE = "DataSamplePayload";
+
+    //private static final String REQUIRED_FILEDS_ONLY = "REQUIRED_FILEDS_ONLY";
+    //private Map<String, ResourceIntegrationTestArtifacts> resourceIntegrationTestArtifactsMap =
+    //        ImmutableMap.of(
+    //                REQUIRED_FILEDS_ONLY, new ResourceIntegrationTestArtifacts(
+    //                        HttpStatus.CREATED,
+    //                        pl -> { return new DataSamplePayload(); },
+    //                        new ArrayList<ConstraintDefinition>()
+    //                )
+    //        );
+
+    private static NewDataSamplePayload xxxx = () -> new DataSamplePayload();
+
+    private final static Map<ResourceIntegrationTestResult, DataSamplePayload> RESOURCE_TESTS = ImmutableMap.of(
+
+            // Create a record with an empty payload
+            (t) -> t.getHttpStatus().equals(HttpStatus.CREATED) &&
+                    t.getConstraintDefinitions().containsAll(new ArrayList<ConstraintDefinition>()), xxxx.create()
+
+    );
 
     @Test
-    public void checkDefaultDataset() {
-        ResponseEntity<DatasetEntityResponse> response = createTestDataset();
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+    public void doit() {
+        NewDataSamplePayload xxxx = () -> new DataSamplePayload();
+
+
+        Dataset defaultDataset = getDefaultDataset();
+        for (Map.Entry<ResourceIntegrationTestResult, DataSamplePayload> es : RESOURCE_TESTS.entrySet()) {
+            ResourceIntegrationTestConditions testResult = new ResourceIntegrationTestConditions(submitPayload(defaultDataset, es.getValue()));
+            Assert.assertTrue(es.getKey().passes(testResult));
+        }
     }
 
-    @Test
-    public void testNullPayload() {
-        ResponseEntity<RecordEntityResponse> recordEntityResponse =
-                submitPayload(getDefaultDataset(), null);
-        Record record = recordEntityResponse.getBody().getData();
-        Assert.assertNull(record.getPayload());
+    @FunctionalInterface
+    public interface ResourceIntegrationTestResult {
+        boolean passes(ResourceIntegrationTestConditions t);
+    }
+
+    @FunctionalInterface
+    public interface NewDataSamplePayload {
+        DataSamplePayload create();
+    }
+
+
+
+
+
+    public class ResourceIntegrationTestConditions {
+        private List<ConstraintDefinition> constraintDefinitions;
+        private HttpStatus httpStatus;
+
+        public ResourceIntegrationTestConditions(ResponseEntity<RecordEntityResponse> recordEntityResponseResponseEntity) {
+            httpStatus = recordEntityResponseResponseEntity.getStatusCode();
+            constraintDefinitions = null;//TODO figure out best way here - do we just request the status?
+        }
+
+        public void setHttpStatus(HttpStatus httpStatus) {
+            this.httpStatus = httpStatus;
+        }
+
+        public List<ConstraintDefinition> getConstraintDefinitions() {
+            return constraintDefinitions;
+        }
+
+        public void setConstraintDefinitions(List<ConstraintDefinition> constraintDefinitions) {
+            this.constraintDefinitions = constraintDefinitions;
+        }
+        public HttpStatus getHttpStatus() {
+            return httpStatus;
+        }
     }
 
     private ResponseEntity<RecordEntityResponse> submitPayload(Dataset dataset, DataSamplePayload payload) {
@@ -61,8 +120,4 @@ public class APIResourceIntegrationTests extends AbstractDataResourceTests {
         DatasetEntityResponse body = response.getBody();
         return body.getData();
     }
-
-
-    //curl -X GET "http://localhost:9020/api/v1/definitions/DataSamplePayload/fields" -H  "accept: application/json"
-    //curl -X GET "http://localhost:9020/api/v1/definitions/DataSamplePayload/constraints" -H  "accept: application/json"
 }
