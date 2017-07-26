b2cab3789d855cc826fcfb21cd311ea221a3e939
Sam Gardner-Dell <sgardnerdell.ea@gmail.com>
2017-07-26 15:56:27 +0100

    Restore dropwizard metrics output

---
+++ src/main/java/uk/gov/ea/datareturns/config/DropWizardMetricsConfiguration.java
@@ -0,0 +1,70 @@
+package uk.gov.ea.datareturns.config;
+
+import com.codahale.metrics.JvmAttributeGaugeSet;
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.jetty9.InstrumentedHandler;
+import com.codahale.metrics.jvm.*;
+import com.ryantenney.metrics.spring.config.annotation.EnableMetrics;
+import com.ryantenney.metrics.spring.config.annotation.MetricsConfigurerAdapter;
+import org.eclipse.jetty.server.Server;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.jetty.JettyEmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.jetty.JettyServerCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configuration for the metrics exposed over the administration port
+ *
+ * @author Sam Gardner-Dell
+ */
+@Configuration
+@EnableMetrics
+public class DropWizardMetricsConfiguration extends MetricsConfigurerAdapter implements JettyServerCustomizer {
+    private InstrumentedHandler metricsHandler;
+
+    /**
+     * Configure the available metrics reporters
+     *
+     * @param metricRegistry the metric registry upon which reporters are configured.
+     */
+    @Override
+    public void configureReporters(final MetricRegistry metricRegistry) {
+        metricRegistry.register("jvm.attribute", new JvmAttributeGaugeSet());
+        metricRegistry.register("jvm.classloader", new ClassLoadingGaugeSet());
+        metricRegistry.register("jvm.filedescriptor", new FileDescriptorRatioGauge());
+        metricRegistry.register("jvm.gc", new GarbageCollectorMetricSet());
+        metricRegistry.register("jvm.memory", new MemoryUsageGaugeSet());
+        metricRegistry.register("jvm.threads", new ThreadStatesGaugeSet());
+        this.metricsHandler = new InstrumentedHandler(metricRegistry, "jetty");
+
+        //    JmxReporter.forRegistry(metricRegistry()).build().start();
+    }
+
+    /**
+     * Bean factory for the {@link EmbeddedServletContainerCustomizer} which allows us to customise the
+     * servlet container
+     *
+     * @return the EmbeddedServletContainerCustomizer
+     */
+    @Bean
+    public EmbeddedServletContainerCustomizer customizer() {
+        return container -> {
+            if (container instanceof JettyEmbeddedServletContainerFactory) {
+                ((JettyEmbeddedServletContainerFactory) container).addServerCustomizers(DropWizardMetricsConfiguration.this);
+            }
+        };
+    }
+
+    /**
+     * Customisation hook for the Jetty server.
+     * Allows us to hook up the metrics system to Jetty
+     *
+     * @param server the Jetty {@link Server} instance
+     */
+    @Override
+    public void customize(final Server server) {
+        this.metricsHandler.setHandler(server.getHandler());
+        server.setHandler(this.metricsHandler);
+    }
+}
