bed8d3963463e31a6d1ef098221cc47f5a93dbd4
Sam Gardner-Dell <sgardnerdell.ea@gmail.com>
2018-01-19 17:34:46 +0000

    Add service-endpoint configuration and hook validation service to configuration

--- common/src/main/java/uk/gov/defra/datareturns/validation/service/ValidationCacheService.java
+++ common/src/main/java/uk/gov/defra/datareturns/validation/service/ValidationCacheService.java
@@ -14,56 +14,59 @@ import org.springframework.hateoas.Resource;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.support.BasicAuthorizationInterceptor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import uk.gov.defra.datareturns.config.ServiceEndpointConfiguration;
 import uk.gov.defra.datareturns.rest.HalRestTemplate;
 
+import java.net.URI;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Provides caching of master data via the spring cache framework for use in validation.
+ *
+ * @author Sam Gardner-Dell
+ */
 public interface ValidationCacheService {
-
-
     /**
      * Get a map from ID to nomenclature for validation
      *
-     * @param resourceCollectionUri
+     * @param collectionResource the collection resource to lookup allowable values
      * @return a {@link Map} from ID to nomenclature
      */
-    Map<String, String> getResourceNomenclatureMap(final String resourceCollectionUri);
-
+    Map<String, String> getResourceNomenclatureMap(final String collectionResource);
 
     @Service
     @Scope(BeanDefinition.SCOPE_SINGLETON)
     @Slf4j
     @RequiredArgsConstructor
     class ValidationCacheServiceImpl implements ValidationCacheService {
+        private final ServiceEndpointConfiguration services;
 
-        @Cacheable(cacheNames = "ValidationCache", key = "#resourceCollectionUri", unless = "#result.isEmpty()")
+        @Cacheable(cacheNames = "ValidationCache", key = "#collectionResource", unless = "#result.isEmpty()")
         @Override
-        public Map<String, String> getResourceNomenclatureMap(final String resourceCollectionUri) {
-            log.info("Fetching resource listing from {}", resourceCollectionUri);
+        public Map<String, String> getResourceNomenclatureMap(final String collectionResource) {
             final Map<String, String> map = new HashMap<>();
-            retrieveAll(resourceCollectionUri).forEach(
+            retrieveAll(collectionResource).forEach(
                     e -> map.put(StringUtils.substringAfterLast(e.getId().getHref(), "/"), e.getContent().getNomenclature())
             );
             return map;
         }
 
-        private Collection<Resource<RestResultEntry>> retrieveAll(final String resourceCollectionUri) {
+        private Collection<Resource<RestResultEntry>> retrieveAll(final String collectionResource) {
             final RestTemplate restTemplate = new HalRestTemplate();
-            // FIXME: Pass through authentication?
-            restTemplate.getInterceptors().add(new BasicAuthorizationInterceptor("user", "password"));
-            // FIXME: URL should not be hardcoded
+            final ServiceEndpointConfiguration.Endpoint endpoint = services.getEndpoint(ServiceEndpointConfiguration.MASTER_DATA_API);
+            endpoint.getAuth().configure(restTemplate);
+            final URI target = endpoint.getUri().resolve(collectionResource);
+            log.info("Fetching resource listing from {}", target);
             final ResponseEntity<PagedResources<Resource<RestResultEntry>>> responseEntity =
-                    restTemplate.exchange("http://localhost:9020/api/" + resourceCollectionUri,
-                            HttpMethod.GET, null,
+                    restTemplate.exchange(
+                            target, HttpMethod.GET, null,
                             new ParameterizedTypeReference<PagedResources<Resource<RestResultEntry>>>() {
-                            },
-                            Collections.emptyMap());
+                            });
             if (responseEntity.getStatusCode() == HttpStatus.OK) {
                 final PagedResources<Resource<RestResultEntry>> entity = responseEntity.getBody();
                 return entity.getContent();
