2a4fa5dc663fd4f90833030891058b2eda321273
Sam Gardner-Dell <sgardnerdell.ea@gmail.com>
2018-01-24 09:48:23 +0000

    Tweak db init sql to remove schema references

--- pi_submissions_api/src/main/resources/db/sql/3-create-database.sql
+++ pi_submissions_api/src/main/resources/db/sql/3-create-database.sql
@@ -3,6 +3,3 @@
  */
 CREATE DATABASE ${db.pi_api.name} OWNER ${db.pi_api.username} ENCODING 'UTF8';
 GRANT ALL PRIVILEGES ON DATABASE ${db.pi_api.name} TO ${db.pi_api.username};
-GRANT USAGE ON SCHEMA PUBLIC TO ${db.pi_api.username};
-GRANT CREATE ON SCHEMA PUBLIC TO ${db.pi_api.username};
-ALTER USER ${db.pi_api.username} SET search_path = public;
